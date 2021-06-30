9f9d55091f5739e3e5a956ab0673f4f6a342253c
Pavel Bonda <bondek777@gmail.com>
2021-06-30 08:37:28 +0300

    Init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useCallback, useRef } from "react";
+import { Container, Jumbotron, Button } from "reactstrap";
 
 function App() {
+  const NO_FILE = "No File Chosen";
+
+  const fileInput = useRef();
+
+  const onChooseFileClick = useCallback(() => {
+    fileInput.current.click();
+  }, []);
+
+  const [fileName, setFileName] = useState(NO_FILE);
+
+  const updateFileName = useCallback((e) => {
+    setFileName(e.target.files[0]?.name || NO_FILE);
+  }, []);
+
+  const splitFile = useCallback(() => {
+    let fileReader = new FileReader();
+    fileReader.onload = (e) => {
+      let lines = e.target.result.split("\n")
+      let headers = lines.shift()
+      console.log(headers)
+      console.log(lines)
+    };
+    fileReader.onerror = () => {
+      alert("Error while reading file.");
+    };
+    fileReader.readAsText(fileInput.current.files[0]);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <Container className="mt-5">
+      <Jumbotron>
+        <h1 className="display-3">CSV Splitter</h1>
+        <p className="lead">
+          Split big .csv file into smaller parts for Excel.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <hr className="mb-4" />
+        <div className="form">
+          <p className="lead">
+            <input
+              type="file"
+              ref={fileInput}
+              accept=".csv"
+              hidden
+              onChange={updateFileName}
+            />
+            <Button
+              color="primary"
+              onClick={onChooseFileClick}
+              className="me-2"
+            >
+              Choose File
+            </Button>
+            {fileName}
+          </p>
+          <p className="lead">
+            <Button
+              color="success"
+              className="me-2"
+              onClick={splitFile}
+              disabled={fileName === NO_FILE}
+            >
+              Split File
+            </Button>
+          </p>
+        </div>
+      </Jumbotron>
+    </Container>
   );
 }
 
