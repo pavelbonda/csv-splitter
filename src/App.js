cc1868af26201fea3cd2ecd60f247835673a55ca
Pavel Bonda <bondek777@gmail.com>
2021-06-30 21:56:06 +0300

    Fix edge line separation && UI updates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import React, { useState, useCallback, useRef } from "react";
-import { Container, Jumbotron, Button } from "reactstrap";
+import { Container, Jumbotron, Button, Alert } from "reactstrap";
 import streamSaver from "streamsaver";
-const NO_FILE = "No File Chosen";
-const ROWS_PER_FILE = 800000;
 
-async function splitFile(file, onFinish) {
+const NO_FILE = "No File Chosen";
+const ROWS_PER_FILE = 900000;
+
+async function splitFile(file, onPartChange, onFinish) {
   const decoder = new TextDecoder();
   const encoder = new TextEncoder();
 
@@ -20,7 +21,7 @@ async function splitFile(file, onFinish) {
     const { done, value } = await reader.read();
 
     if (done) {
-      onFinish(false);
+      onFinish();
 
       if (writer) {
         writer.close();
@@ -29,7 +30,10 @@ async function splitFile(file, onFinish) {
       break;
     }
 
-    let rows = decoder.decode(value).split("\n");
+    let textChunk = decoder.decode(value);
+    let isChunkEndsWithNewLine = textChunk.slice(-1) === "\n";
+
+    let rows = textChunk.split("\n");
 
     if (!headers) {
       headers = rows.shift();
@@ -43,14 +47,26 @@ async function splitFile(file, onFinish) {
 
       if (currentRow === 0) {
         currentPart += 1;
+
+        onPartChange(currentPart);
+
         let fileStream = streamSaver.createWriteStream(
-          `${file.name.split(".")[0]}_part_${currentPart}.csv`
+          `${file.name
+            .split(".")
+            .slice(0, -1)
+            .join(".")}_part_${currentPart}.csv`
         );
+        
         writer = fileStream.getWriter();
         await writer.write(encoder.encode(`${headers}\n`));
       }
 
-      await writer.write(encoder.encode(`${rows[i]}\n`));
+      if (i === rows.length - 1 && !isChunkEndsWithNewLine) {
+        await writer.write(encoder.encode(`${rows[i]}`));
+      } else {
+        await writer.write(encoder.encode(`${rows[i]}\n`));
+      }
+
       currentRow += 1;
     }
   }
@@ -59,62 +75,80 @@ async function splitFile(file, onFinish) {
 function App() {
   const fileInput = useRef();
 
-  const [fileName, setFileName] = useState(NO_FILE);
   const [isProcessing, setIsProcessing] = useState(false);
+  const [currentPart, setCurrentPart] = useState(null);
+  const [file, setFile] = useState(null);
 
   const onChooseFileClick = useCallback(() => {
     fileInput.current.click();
   }, []);
 
-  const updateFileName = useCallback((e) => {
-    setFileName(e.target.files[0]?.name || NO_FILE);
-  }, []);
+  const onFileChange = useCallback(
+    (e) => {
+      setFile(e.target.files[0]);
+    },
+    [setFile]
+  );
 
   const startProcessing = useCallback(() => {
     setIsProcessing(true);
-    splitFile(fileInput.current.files[0], () => {
-      setIsProcessing(false);
-    });
-  }, []);
+    splitFile(file, setCurrentPart, () => setIsProcessing(false));
+  }, [file, setCurrentPart]);
 
   return (
     <Container className="mt-5">
       <Jumbotron>
-        <h1 className="display-3">CSV Splitter</h1>
+        <h1 className="display-3">CSV Splitter for Excel</h1>
+        <p className="lead mb-0">
+          Excel has a limit of a maximum 1 million rows per file.
+        </p>
         <p className="lead">
-          Split big .csv file into smaller parts for Excel.
+          This website will split your huge .CSV file into smaller files that
+          Excel can handle.
+        </p>
+        <p className="lead">
+          <b>
+            Please, allow pop-up's since you will be asked to download multiple
+            files.
+          </b>
         </p>
         <hr className="mb-4" />
-        <div className="form">
-          <p className="lead">
-            <input
-              type="file"
-              ref={fileInput}
-              accept=".csv"
-              hidden
-              onChange={updateFileName}
-            />
-            <Button
-              color="primary"
-              onClick={onChooseFileClick}
-              className="me-2"
-              disabled={isProcessing}
-            >
-              Choose File
-            </Button>
-            {fileName}
-          </p>
-          <p className="lead">
-            <Button
-              color="success"
-              className="me-2"
-              onClick={startProcessing}
-              disabled={fileName === NO_FILE || isProcessing}
-            >
-              Split File
-            </Button>
-          </p>
-        </div>
+        {!isProcessing && (
+          <div>
+            <p className="lead">
+              <input
+                type="file"
+                ref={fileInput}
+                accept=".csv"
+                hidden
+                onChange={onFileChange}
+              />
+              <Button
+                color="primary"
+                onClick={onChooseFileClick}
+                className="me-2"
+              >
+                Choose File
+              </Button>
+              {file?.name || NO_FILE}
+            </p>
+            <p className="lead">
+              <Button
+                color="success"
+                className="me-2"
+                onClick={startProcessing}
+                disabled={!file}
+              >
+                Split File
+              </Button>
+            </p>
+          </div>
+        )}
+        {isProcessing && (
+          <Alert color="primary">
+            Downloading part {currentPart}. Please, don't close this tab.
+          </Alert>
+        )}
       </Jumbotron>
     </Container>
   );
