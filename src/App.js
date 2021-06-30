c91047542997e18b80beaf38258f3ecb9b0d38db
Pavel Bonda <bondek777@gmail.com>
2021-06-30 18:31:03 +0300

    Implement splitter using streams

--- src/App.js
+++ src/App.js
@@ -1,34 +1,80 @@
 import "./App.css";
 import React, { useState, useCallback, useRef } from "react";
 import { Container, Jumbotron, Button } from "reactstrap";
+import streamSaver from "streamsaver";
+const NO_FILE = "No File Chosen";
+const ROWS_PER_FILE = 800000;
+
+async function splitFile(file, onFinish) {
+  const decoder = new TextDecoder();
+  const encoder = new TextEncoder();
+
+  let reader = file.stream().getReader();
+
+  let headers = null;
+  let currentPart = 0;
+  let currentRow = 0;
+  let writer = null;
+
+  while (true) {
+    const { done, value } = await reader.read();
+
+    if (done) {
+      onFinish(false);
+
+      if (writer) {
+        writer.close();
+      }
+
+      break;
+    }
+
+    let rows = decoder.decode(value).split("\n");
+
+    if (!headers) {
+      headers = rows.shift();
+    }
+
+    for (let i = 0; i < rows.length; i++) {
+      if (currentRow >= ROWS_PER_FILE) {
+        currentRow = 0;
+        writer.close();
+      }
+
+      if (currentRow === 0) {
+        currentPart += 1;
+        let fileStream = streamSaver.createWriteStream(
+          `${file.name.split(".")[0]}_part_${currentPart}.csv`
+        );
+        writer = fileStream.getWriter();
+        await writer.write(encoder.encode(`${headers}\n`));
+      }
+
+      await writer.write(encoder.encode(`${rows[i]}\n`));
+      currentRow += 1;
+    }
+  }
+}
 
 function App() {
-  const NO_FILE = "No File Chosen";
-
   const fileInput = useRef();
+
+  const [fileName, setFileName] = useState(NO_FILE);
+  const [isProcessing, setIsProcessing] = useState(false);
 
   const onChooseFileClick = useCallback(() => {
     fileInput.current.click();
   }, []);
 
-  const [fileName, setFileName] = useState(NO_FILE);
-
   const updateFileName = useCallback((e) => {
     setFileName(e.target.files[0]?.name || NO_FILE);
   }, []);
 
-  const splitFile = useCallback(() => {
-    let fileReader = new FileReader();
-    fileReader.onload = (e) => {
-      let lines = e.target.result.split("\n")
-      let headers = lines.shift()
-      console.log(headers)
-      console.log(lines)
-    };
-    fileReader.onerror = () => {
-      alert("Error while reading file.");
-    };
-    fileReader.readAsText(fileInput.current.files[0]);
+  const startProcessing = useCallback(() => {
+    setIsProcessing(true);
+    splitFile(fileInput.current.files[0], () => {
+      setIsProcessing(false);
+    });
   }, []);
 
   return (
@@ -52,6 +98,7 @@ function App() {
               color="primary"
               onClick={onChooseFileClick}
               className="me-2"
+              disabled={isProcessing}
             >
               Choose File
             </Button>
@@ -61,8 +108,8 @@ function App() {
             <Button
               color="success"
               className="me-2"
-              onClick={splitFile}
-              disabled={fileName === NO_FILE}
+              onClick={startProcessing}
+              disabled={fileName === NO_FILE || isProcessing}
             >
               Split File
             </Button>
